c4718758d527912ef3ca5d8b203ed9d877d63783
Rafael Olaechea <rolaechea@gsd.uwaterloo.ca>
2018-11-22 15:22:25 -0500

    Fixed code for replicability

--- run_6_to_10.sh
+++
@@ -1,5 +0,0 @@
-python IntegratedSampledDataCreation.py 460 train_conf_460_rep_6.pkl sampled_460_rep_6.pkl ; python AnalyzedSampledData.py sampled_460_rep_6.pkl train_conf_460_rep_6.pkl > results_460_rep_6.csv
-python IntegratedSampledDataCreation.py 460 train_conf_460_rep_7.pkl sampled_460_rep_7.pkl ; python AnalyzedSampledData.py sampled_460_rep_7.pkl train_conf_460_rep_7.pkl > results_460_rep_7.csv
-python IntegratedSampledDataCreation.py 460 train_conf_460_rep_8.pkl sampled_460_rep_8.pkl ; python AnalyzedSampledData.py sampled_460_rep_8.pkl train_conf_460_rep_8.pkl > results_460_rep_8.csv
-python IntegratedSampledDataCreation.py 460 train_conf_460_rep_9.pkl sampled_460_rep_9.pkl ; python AnalyzedSampledData.py sampled_460_rep_9.pkl train_conf_460_rep_9.pkl > results_460_rep_9.csv
-python IntegratedSampledDataCreation.py 460 train_conf_460_rep_10.pkl sampled_460_rep_10.pkl ; python AnalyzedSampledData.py sampled_460_rep_10.pkl train_conf_460_rep_10.pkl > results_460_rep_10.csv
