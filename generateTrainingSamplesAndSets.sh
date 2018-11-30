6255658472bb582008bcdcbccdb389bee434b1cd
Rafael Olaechea <rolaechea@gsd.uwaterloo.ca>
2018-11-30 15:08:59 -0500

    Code for sampling training data

--- generateTrainingSamplesAndSets.sh
+++ generateTrainingSamplesAndSets.sh
@@ -1,11 +1,11 @@
-python IntegratedSampledDataCreation.py 460 train_conf_460_rep_1.pkl sampled_460_rep_1.pkl  
-python IntegratedSampledDataCreation.py 460 train_conf_460_rep_2.pkl sampled_460_rep_2.pkl  
-python IntegratedSampledDataCreation.py 460 train_conf_460_rep_3.pkl sampled_460_rep_3.pkl  
-python IntegratedSampledDataCreation.py 460 train_conf_460_rep_4.pkl sampled_460_rep_4.pkl
-python IntegratedSampledDataCreation.py 460 train_conf_460_rep_5.pkl sampled_460_rep_5.pkl
-python IntegratedSampledDataCreation.py 460 train_conf_460_rep_6.pkl sampled_460_rep_6.pkl  
-python IntegratedSampledDataCreation.py 460 train_conf_460_rep_7.pkl sampled_460_rep_7.pkl 
-python IntegratedSampledDataCreation.py 460 train_conf_460_rep_8.pkl sampled_460_rep_8.pkl 
-python IntegratedSampledDataCreation.py 460 train_conf_460_rep_9.pkl sampled_460_rep_9.pkl  
-python IntegratedSampledDataCreation.py 460 train_conf_460_rep_10.pkl sampled_460_rep_10.pkl  
+python IntegratedSampledDataCreation.py 460 train_conf_460_rep_1.pkl sampled_460_rep_1.pkl  debug ; rm tmpFile_*pkl
+python IntegratedSampledDataCreation.py 460 train_conf_460_rep_2.pkl sampled_460_rep_2.pkl  debug ; rm tmpFile_*pkl
+python IntegratedSampledDataCreation.py 460 train_conf_460_rep_3.pkl sampled_460_rep_3.pkl  debug ; rm tmpFile_*pkl
+python IntegratedSampledDataCreation.py 460 train_conf_460_rep_4.pkl sampled_460_rep_4.pkl  debug ; rm tmpFile_*pkl
+python IntegratedSampledDataCreation.py 460 train_conf_460_rep_5.pkl sampled_460_rep_5.pkl  debug ; rm tmpFile_*pkl
+python IntegratedSampledDataCreation.py 460 train_conf_460_rep_6.pkl sampled_460_rep_6.pkl  debug ; rm tmpFile_*pkl
+python IntegratedSampledDataCreation.py 460 train_conf_460_rep_7.pkl sampled_460_rep_7.pkl debug ; rm tmpFile_*pkl
+python IntegratedSampledDataCreation.py 460 train_conf_460_rep_8.pkl sampled_460_rep_8.pkl  debug ; rm tmpFile_*pkl
+python IntegratedSampledDataCreation.py 460 train_conf_460_rep_9.pkl sampled_460_rep_9.pkl  debug ; rm tmpFile_*pkl
+python IntegratedSampledDataCreation.py 460 train_conf_460_rep_10.pkl sampled_460_rep_10.pkl debug ; rm tmpFile_*pkl 
 
