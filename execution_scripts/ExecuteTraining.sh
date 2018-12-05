bbba6928516af0f30515f8746740f1a04490894d
Rafael Olaechea <rolaechea@gsd.uwaterloo.ca>
2018-12-05 13:07:31 -0500

    unifying scripts

--- execution_scripts/ExecuteTraining.sh
+++ execution_scripts/ExecuteTraining.sh
@@ -8,3 +8,13 @@ python AnalyzedSampledData.py train_conf_460_rep_7.pkl sampled_460_rep_7.pkl > r
 python AnalyzedSampledData.py train_conf_460_rep_8.pkl sampled_460_rep_8.pkl > results_460_rep_8.csv
 python AnalyzedSampledData.py train_conf_460_rep_9.pkl sampled_460_rep_9.pkl > results_460_rep_9.csv
 python AnalyzedSampledData.py train_conf_460_rep_10.pkl sampled_460_rep_10.pkl > results_460_rep_10.csv
+cat results_460_rep_1.csv | cut -d, -f1,91,92 > best_method_460_rep_1.csv
+cat results_460_rep_2.csv | cut -d, -f1,91,92 > best_method_460_rep_2.csv
+cat results_460_rep_3.csv | cut -d, -f1,91,92 >  best_method_460_rep_3.csv
+cat results_460_rep_4.csv | cut -d, -f1,91,92 > best_method_460_rep_4.csv
+cat results_460_rep_5.csv | cut -d, -f1,91,92 > best_method_460_rep_5.csv
+cat results_460_rep_6.csv | cut -d, -f1,91,92 > best_method_460_rep_6.csv
+cat results_460_rep_7.csv | cut -d, -f1,91,92 > best_method_460_rep_7.csv
+cat results_460_rep_8.csv | cut -d, -f1,91,92 > best_method_460_rep_8.csv
+cat results_460_rep_9.csv | cut -d, -f1,91,92 > best_method_460_rep_9.csv
+cat results_460_rep_10.csv | cut -d, -f1,91,92 > best_method_460_rep_10.csv
