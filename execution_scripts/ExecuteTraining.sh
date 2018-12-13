b4cb7d113aa580d63546d227f41f0ce9eb43a6f1
Rafael Olaechea <rolaechea@gsd.uwaterloo.ca>
2018-12-13 17:12:14 -0500

    Generalizing scripts

--- execution_scripts/ExecuteTraining.sh
+++ execution_scripts/ExecuteTraining.sh
@@ -1,20 +1,20 @@
-python LearnWithCrossValidation.py train_conf_460_rep_1.pkl sampled_460_rep_1.pkl > results_460_rep_1.csv
-python LearnWithCrossValidation.py train_conf_460_rep_2.pkl sampled_460_rep_2.pkl > results_460_rep_2.csv
-python LearnWithCrossValidation.py train_conf_460_rep_3.pkl sampled_460_rep_3.pkl > results_460_rep_3.csv
-python LearnWithCrossValidation.py train_conf_460_rep_4.pkl sampled_460_rep_4.pkl > results_460_rep_4.csv
-python LearnWithCrossValidation.py train_conf_460_rep_5.pkl sampled_460_rep_5.pkl > results_460_rep_5.csv
-python LearnWithCrossValidation.py train_conf_460_rep_6.pkl sampled_460_rep_6.pkl > results_460_rep_6.csv
-python LearnWithCrossValidation.py train_conf_460_rep_7.pkl sampled_460_rep_7.pkl > results_460_rep_7.csv
-python LearnWithCrossValidation.py train_conf_460_rep_8.pkl sampled_460_rep_8.pkl > results_460_rep_8.csv
-python LearnWithCrossValidation.py train_conf_460_rep_9.pkl sampled_460_rep_9.pkl > results_460_rep_9.csv
-python LearnWithCrossValidation.py train_conf_460_rep_10.pkl sampled_460_rep_10.pkl > results_460_rep_10.csv
-cat results_460_rep_1.csv | cut -d, -f1,91,92 > best_method_460_rep_1.csv
-cat results_460_rep_2.csv | cut -d, -f1,91,92 > best_method_460_rep_2.csv
-cat results_460_rep_3.csv | cut -d, -f1,91,92 >  best_method_460_rep_3.csv
-cat results_460_rep_4.csv | cut -d, -f1,91,92 > best_method_460_rep_4.csv
-cat results_460_rep_5.csv | cut -d, -f1,91,92 > best_method_460_rep_5.csv
-cat results_460_rep_6.csv | cut -d, -f1,91,92 > best_method_460_rep_6.csv
-cat results_460_rep_7.csv | cut -d, -f1,91,92 > best_method_460_rep_7.csv
-cat results_460_rep_8.csv | cut -d, -f1,91,92 > best_method_460_rep_8.csv
-cat results_460_rep_9.csv | cut -d, -f1,91,92 > best_method_460_rep_9.csv
-cat results_460_rep_10.csv | cut -d, -f1,91,92 > best_method_460_rep_10.csv
+python LearnWithCrossValidation.py $1 $2train_conf_rep_1.pkl $2sampled_rep_1.pkl > $3results_rep_1.csv
+python LearnWithCrossValidation.py $1 $2train_conf_rep_2.pkl $2sampled_rep_2.pkl > $3results_rep_2.csv
+python LearnWithCrossValidation.py $1 $2train_conf_rep_3.pkl $2sampled_rep_3.pkl > $3results_rep_3.csv
+python LearnWithCrossValidation.py $1 $2train_conf_rep_4.pkl $2sampled_rep_4.pkl > $3results_rep_4.csv
+python LearnWithCrossValidation.py $1 $2train_conf_rep_5.pkl $2sampled_rep_5.pkl > $3results_rep_5.csv
+python LearnWithCrossValidation.py $1 $2train_conf_rep_6.pkl $2sampled_rep_6.pkl > $3results_rep_6.csv
+python LearnWithCrossValidation.py $1 $2train_conf_rep_7.pkl $2sampled_rep_7.pkl > $3results_rep_7.csv
+python LearnWithCrossValidation.py $1 $2train_conf_rep_8.pkl $2sampled_rep_8.pkl > $3results_rep_8.csv
+python LearnWithCrossValidation.py $1 $2train_conf_rep_9.pkl $2sampled_rep_9.pkl > $3results_rep_9.csv
+python LearnWithCrossValidation.py $1 $2train_conf_rep_10.pkl $2sampled_rep_10.pkl >  $3results_rep_10.csv
+cat $3results_rep_1.csv | cut -d, -f1,91,92 > $3best_method_rep_1.csv
+cat $3results_rep_2.csv | cut -d, -f1,91,92 > $3best_method_rep_2.csv
+cat $3results_rep_3.csv | cut -d, -f1,91,92 >  $3best_method_rep_3.csv
+cat $3results_rep_4.csv | cut -d, -f1,91,92 > $3best_method_rep_4.csv
+cat $3results_rep_5.csv | cut -d, -f1,91,92 > $3best_method_rep_5.csv
+cat $3results_rep_6.csv | cut -d, -f1,91,92 > $3best_method_rep_6.csv
+cat $3results_rep_7.csv | cut -d, -f1,91,92 > $3best_method_rep_7.csv
+cat $3results_rep_8.csv | cut -d, -f1,91,92 > $3best_method_rep_8.csv
+cat $3results_rep_9.csv | cut -d, -f1,91,92 > $3best_method_rep_9.csv
+cat $3results_rep_10.csv | cut -d, -f1,91,92 > $3best_method_rep_10.csv
