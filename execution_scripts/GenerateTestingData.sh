cc92503cfdfb6a5b1e6b8ae35121c4251d3f8809
Rafael Olaechea <rolaechea@gsd.uwaterloo.ca>
2018-12-14 11:59:58 -0500

    making code to generate testing data generic

--- execution_scripts/GenerateTestingData.sh
+++ execution_scripts/GenerateTestingData.sh
@@ -1,10 +1,12 @@
-python GenerateSampledTestSet.py $1train_conf_rep_1.pkl $1test_conf_rep_1.pkl  $1test_sampled_rep_1.pkl
-python GenerateSampledTestSet.py $1train_conf_rep_2.pkl $1test_conf_rep_2.pkl  $1test_sampled_rep_2.pkl $1test_conf_rep_1.pkl $1test_sampled_rep_1.pkl 
-python GenerateSampledTestSet.py $1train_conf_rep_3.pkl $1test_conf_rep_3.pkl  $1test_sampled_rep_3.pkl $1test_conf_rep_1.pkl $1test_sampled_rep_1.pkl $1test_conf_rep_2.pkl $1test_sampled_rep_2.pkl
-python GenerateSampledTestSet.py $1train_conf_rep_4.pkl $1test_conf_rep_4.pkl  $1test_sampled_rep_4.pkl $1test_conf_rep_1.pkl $1test_sampled_rep_1.pkl $1test_conf_rep_2.pkl $1test_sampled_rep_2.pkl
-python GenerateSampledTestSet.py $1train_conf_rep_5.pkl $1test_conf_rep_5.pkl  $1test_sampled_rep_5.pkl $1test_conf_rep_1.pkl $1test_sampled_rep_1.pkl $1test_conf_rep_2.pkl $1test_sampled_rep_2.pkl
-python GenerateSampledTestSet.py $1train_conf_rep_6.pkl $1test_conf_rep_6.pkl  $1test_sampled_rep_6.pkl $1test_conf_rep_1.pkl $1test_sampled_rep_1.pkl $1test_conf_rep_2.pkl $1test_sampled_rep_2.pkl
-python GenerateSampledTestSet.py $1train_conf_rep_7.pkl $1test_conf_rep_7.pkl  $1test_sampled_rep_7.pkl $1test_conf_rep_1.pkl $1test_sampled_rep_1.pkl $1test_conf_rep_2.pkl $1test_sampled_rep_2.pkl
-python GenerateSampledTestSet.py $1train_conf_rep_8.pkl $1test_conf_rep_8.pkl  $1test_sampled_rep_8.pkl $1test_conf_rep_1.pkl $1test_sampled_rep_1.pkl $1test_conf_rep_2.pkl $1test_sampled_rep_2.pkl
-python GenerateSampledTestSet.py $1train_conf_rep_9.pkl $1test_conf_rep_9.pkl  $1test_sampled_rep_9.pkl $1test_conf_rep_1.pkl $1test_sampled_rep_1.pkl $1test_conf_rep_2.pkl $1test_sampled_rep_2.pkl
-python GenerateSampledTestSet.py $1train_conf_rep_10.pkl $1test_conf_rep_10.pkl  $1test_sampled_rep_10.pkl $1test_conf_rep_1.pkl $1test_sampled_rep_1.pkl $1test_conf_rep_2.pkl $1test_sampled_rep_2.pkl
+#
+#For autonomoose GenerateSampledTestSetpy $1 wil only use first 3 arguments.
+python GenerateSampledTestSetpy $1 $2train_conf_rep_1.pkl $2test_conf_rep_1.pkl  $2test_sampled_rep_1.pkl
+python GenerateSampledTestSetpy $1 $2train_conf_rep_2.pkl $2test_conf_rep_2.pkl  $2test_sampled_rep_2.pkl $2test_conf_rep_1.pkl $2test_sampled_rep_1.pkl 
+python GenerateSampledTestSetpy $1 $2train_conf_rep_3.pkl $2test_conf_rep_3.pkl  $2test_sampled_rep_3.pkl $2test_conf_rep_1.pkl $2test_sampled_rep_1.pkl $2test_conf_rep_2.pkl $2test_sampled_rep_2.pkl
+python GenerateSampledTestSetpy $1 $2train_conf_rep_4.pkl $2test_conf_rep_4.pkl  $2test_sampled_rep_4.pkl $2test_conf_rep_1.pkl $2test_sampled_rep_1.pkl $2test_conf_rep_2.pkl $2test_sampled_rep_2.pkl
+python GenerateSampledTestSetpy $1 $2train_conf_rep_5.pkl $2test_conf_rep_5.pkl  $2test_sampled_rep_5.pkl $2test_conf_rep_1.pkl $2test_sampled_rep_1.pkl $2test_conf_rep_2.pkl $2test_sampled_rep_2.pkl
+python GenerateSampledTestSetpy $1 $2train_conf_rep_6.pkl $2test_conf_rep_6.pkl  $2test_sampled_rep_6.pkl $2test_conf_rep_1.pkl $2test_sampled_rep_1.pkl $2test_conf_rep_2.pkl $2test_sampled_rep_2.pkl
+python GenerateSampledTestSetpy $1 $2train_conf_rep_7.pkl $2test_conf_rep_7.pkl  $2test_sampled_rep_7.pkl $2test_conf_rep_1.pkl $2test_sampled_rep_1.pkl $2test_conf_rep_2.pkl $2test_sampled_rep_2.pkl
+python GenerateSampledTestSetpy $1 $2train_conf_rep_8.pkl $2test_conf_rep_8.pkl  $2test_sampled_rep_8.pkl $2test_conf_rep_1.pkl $2test_sampled_rep_1.pkl $2test_conf_rep_2.pkl $2test_sampled_rep_2.pkl
+python GenerateSampledTestSetpy $1 $2train_conf_rep_9.pkl $2test_conf_rep_9.pkl  $2test_sampled_rep_9.pkl $2test_conf_rep_1.pkl $2test_sampled_rep_1.pkl $2test_conf_rep_2.pkl $2test_sampled_rep_2.pkl
+python GenerateSampledTestSetpy $1 $2train_conf_rep_10.pkl $2test_conf_rep_10.pkl  $2test_sampled_rep_10.pkl $2test_conf_rep_1.pkl $2test_sampled_rep_1.pkl $2test_conf_rep_2.pkl $2test_sampled_rep_2.pkl
