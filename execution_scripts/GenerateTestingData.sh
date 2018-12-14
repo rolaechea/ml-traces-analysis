2a6139a29b17ce0a6f3cd82f8aae5c2d5b3ff16e
Rafael Olaechea <rolaechea@gsd.uwaterloo.ca>
2018-12-14 11:33:25 -0500

    removing 460

--- execution_scripts/GenerateTestingData.sh
+++ execution_scripts/GenerateTestingData.sh
@@ -1,10 +1,10 @@
-python GenerateSampledTestSet.py $1train_conf_460_rep_1.pkl $1test_conf_460_rep_1.pkl  $1test_sampled_460_rep_1.pkl
-python GenerateSampledTestSet.py $1train_conf_460_rep_2.pkl $1test_conf_460_rep_2.pkl  $1test_sampled_460_rep_2.pkl $1test_conf_460_rep_1.pkl $1test_sampled_460_rep_1.pkl 
-python GenerateSampledTestSet.py $1train_conf_460_rep_3.pkl $1test_conf_460_rep_3.pkl  $1test_sampled_460_rep_3.pkl $1test_conf_460_rep_1.pkl $1test_sampled_460_rep_1.pkl $1test_conf_460_rep_2.pkl $1test_sampled_460_rep_2.pkl
-python GenerateSampledTestSet.py $1train_conf_460_rep_4.pkl $1test_conf_460_rep_4.pkl  $1test_sampled_460_rep_4.pkl $1test_conf_460_rep_1.pkl $1test_sampled_460_rep_1.pkl $1test_conf_460_rep_2.pkl $1test_sampled_460_rep_2.pkl
-python GenerateSampledTestSet.py $1train_conf_460_rep_5.pkl $1test_conf_460_rep_5.pkl  $1test_sampled_460_rep_5.pkl $1test_conf_460_rep_1.pkl $1test_sampled_460_rep_1.pkl $1test_conf_460_rep_2.pkl $1test_sampled_460_rep_2.pkl
-python GenerateSampledTestSet.py $1train_conf_460_rep_6.pkl $1test_conf_460_rep_6.pkl  $1test_sampled_460_rep_6.pkl $1test_conf_460_rep_1.pkl $1test_sampled_460_rep_1.pkl $1test_conf_460_rep_2.pkl $1test_sampled_460_rep_2.pkl
-python GenerateSampledTestSet.py $1train_conf_460_rep_7.pkl $1test_conf_460_rep_7.pkl  $1test_sampled_460_rep_7.pkl $1test_conf_460_rep_1.pkl $1test_sampled_460_rep_1.pkl $1test_conf_460_rep_2.pkl $1test_sampled_460_rep_2.pkl
-python GenerateSampledTestSet.py $1train_conf_460_rep_8.pkl $1test_conf_460_rep_8.pkl  $1test_sampled_460_rep_8.pkl $1test_conf_460_rep_1.pkl $1test_sampled_460_rep_1.pkl $1test_conf_460_rep_2.pkl $1test_sampled_460_rep_2.pkl
-python GenerateSampledTestSet.py $1train_conf_460_rep_9.pkl $1test_conf_460_rep_9.pkl  $1test_sampled_460_rep_9.pkl $1test_conf_460_rep_1.pkl $1test_sampled_460_rep_1.pkl $1test_conf_460_rep_2.pkl $1test_sampled_460_rep_2.pkl
-python GenerateSampledTestSet.py $1train_conf_460_rep_10.pkl $1test_conf_460_rep_10.pkl  $1test_sampled_460_rep_10.pkl $1test_conf_460_rep_1.pkl $1test_sampled_460_rep_1.pkl $1test_conf_460_rep_2.pkl $1test_sampled_460_rep_2.pkl
+python GenerateSampledTestSet.py $1train_conf_rep_1.pkl $1test_conf_rep_1.pkl  $1test_sampled_rep_1.pkl
+python GenerateSampledTestSet.py $1train_conf_rep_2.pkl $1test_conf_rep_2.pkl  $1test_sampled_rep_2.pkl $1test_conf_rep_1.pkl $1test_sampled_rep_1.pkl 
+python GenerateSampledTestSet.py $1train_conf_rep_3.pkl $1test_conf_rep_3.pkl  $1test_sampled_rep_3.pkl $1test_conf_rep_1.pkl $1test_sampled_rep_1.pkl $1test_conf_rep_2.pkl $1test_sampled_rep_2.pkl
+python GenerateSampledTestSet.py $1train_conf_rep_4.pkl $1test_conf_rep_4.pkl  $1test_sampled_rep_4.pkl $1test_conf_rep_1.pkl $1test_sampled_rep_1.pkl $1test_conf_rep_2.pkl $1test_sampled_rep_2.pkl
+python GenerateSampledTestSet.py $1train_conf_rep_5.pkl $1test_conf_rep_5.pkl  $1test_sampled_rep_5.pkl $1test_conf_rep_1.pkl $1test_sampled_rep_1.pkl $1test_conf_rep_2.pkl $1test_sampled_rep_2.pkl
+python GenerateSampledTestSet.py $1train_conf_rep_6.pkl $1test_conf_rep_6.pkl  $1test_sampled_rep_6.pkl $1test_conf_rep_1.pkl $1test_sampled_rep_1.pkl $1test_conf_rep_2.pkl $1test_sampled_rep_2.pkl
+python GenerateSampledTestSet.py $1train_conf_rep_7.pkl $1test_conf_rep_7.pkl  $1test_sampled_rep_7.pkl $1test_conf_rep_1.pkl $1test_sampled_rep_1.pkl $1test_conf_rep_2.pkl $1test_sampled_rep_2.pkl
+python GenerateSampledTestSet.py $1train_conf_rep_8.pkl $1test_conf_rep_8.pkl  $1test_sampled_rep_8.pkl $1test_conf_rep_1.pkl $1test_sampled_rep_1.pkl $1test_conf_rep_2.pkl $1test_sampled_rep_2.pkl
+python GenerateSampledTestSet.py $1train_conf_rep_9.pkl $1test_conf_rep_9.pkl  $1test_sampled_rep_9.pkl $1test_conf_rep_1.pkl $1test_sampled_rep_1.pkl $1test_conf_rep_2.pkl $1test_sampled_rep_2.pkl
+python GenerateSampledTestSet.py $1train_conf_rep_10.pkl $1test_conf_rep_10.pkl  $1test_sampled_rep_10.pkl $1test_conf_rep_1.pkl $1test_sampled_rep_1.pkl $1test_conf_rep_2.pkl $1test_sampled_rep_2.pkl
