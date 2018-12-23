0b56f1d1a17bce42205a14399fed85a04ced022f
Rafael Olaechea <rolaechea@gsd.uwaterloo.ca>
2018-12-23 10:26:45 -0500

    generalizing scripts for generating excel

--- excel_scripts/calc_test_csv_for_all.sh
+++ excel_scripts/calc_test_csv_for_all.sh
@@ -3,5 +3,5 @@ transitions='4 5 6 7 8 9 10 11 12 13 14 15 18 19 20 21 23 24 25 28 33 34'
 for value in $transitions 
 
 do
-    bash preComputeForTransition.sh $value
+    bash excel_scripts/preComputeForTransition.sh $value $1
 done
