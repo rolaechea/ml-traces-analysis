64859ebedcaa3c47ba35a2083c9d3d756b8e10e1
Rafael Olaechea <rolaechea@gsd.uwaterloo.ca>
2019-02-04 17:02:13 -0500

    script to run akiyo

---
+++ execution_scripts/analyzeRQ2FseX264-akiyo.sh
@@ -0,0 +1,10 @@
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 akiyo/train_conf_rep_1.pkl akiyo/test_conf_rep_1.pkl akiyo/traceExecutionTimesForAll.pkl > akiyo/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 akiyo/train_conf_rep_2.pkl akiyo/test_conf_rep_2.pkl akiyo/traceExecutionTimesForAll.pkl | tail -n1 >> akiyo/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 akiyo/train_conf_rep_3.pkl akiyo/test_conf_rep_3.pkl akiyo/traceExecutionTimesForAll.pkl | tail -n1  >> akiyo/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 akiyo/train_conf_rep_4.pkl akiyo/test_conf_rep_4.pkl akiyo/traceExecutionTimesForAll.pkl | tail -n1 >> akiyo/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 akiyo/train_conf_rep_5.pkl akiyo/test_conf_rep_5.pkl akiyo/traceExecutionTimesForAll.pkl | tail -n1  >> akiyo/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 akiyo/train_conf_rep_6.pkl akiyo/test_conf_rep_6.pkl akiyo/traceExecutionTimesForAll.pkl | tail -n1  >> akiyo/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 akiyo/train_conf_rep_7.pkl akiyo/test_conf_rep_7.pkl akiyo/traceExecutionTimesForAll.pkl | tail -n1  >> akiyo/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 akiyo/train_conf_rep_8.pkl akiyo/test_conf_rep_8.pkl akiyo/traceExecutionTimesForAll.pkl | tail -n1  >> akiyo/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 akiyo/train_conf_rep_9.pkl akiyo/test_conf_rep_9.pkl akiyo/traceExecutionTimesForAll.pkl | tail -n1  >> akiyo/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 akiyo/train_conf_rep_10.pkl akiyo/test_conf_rep_10.pkl akiyo/traceExecutionTimesForAll.pkl | tail -n1  >> akiyo/results/rq2_fse_control.csv
