eec879e97f5c12d092c21a27ad51f4a41b0a4b25
Rafael Olaechea <rolaechea@gsd.uwaterloo.ca>
2019-02-06 10:49:45 -0500

    code to run FSE control

---
+++ execution_scripts/analyzeRQ2FseX264-news.sh
@@ -0,0 +1,10 @@
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 news/train_conf_rep_1.pkl news/test_conf_rep_1.pkl news/traceExecutionTimesForAll.pkl > news/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 news/train_conf_rep_2.pkl news/test_conf_rep_2.pkl news/traceExecutionTimesForAll.pkl | tail -n1 >> news/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 news/train_conf_rep_3.pkl news/test_conf_rep_3.pkl news/traceExecutionTimesForAll.pkl | tail -n1  >> news/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 news/train_conf_rep_4.pkl news/test_conf_rep_4.pkl news/traceExecutionTimesForAll.pkl | tail -n1 >> news/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 news/train_conf_rep_5.pkl news/test_conf_rep_5.pkl news/traceExecutionTimesForAll.pkl | tail -n1  >> news/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 news/train_conf_rep_6.pkl news/test_conf_rep_6.pkl news/traceExecutionTimesForAll.pkl | tail -n1  >> news/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 news/train_conf_rep_7.pkl news/test_conf_rep_7.pkl news/traceExecutionTimesForAll.pkl | tail -n1  >> news/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 news/train_conf_rep_8.pkl news/test_conf_rep_8.pkl news/traceExecutionTimesForAll.pkl | tail -n1  >> news/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 news/train_conf_rep_9.pkl news/test_conf_rep_9.pkl news/traceExecutionTimesForAll.pkl | tail -n1  >> news/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 news/train_conf_rep_10.pkl news/test_conf_rep_10.pkl news/traceExecutionTimesForAll.pkl | tail -n1  >> news/results/rq2_fse_control.csv
