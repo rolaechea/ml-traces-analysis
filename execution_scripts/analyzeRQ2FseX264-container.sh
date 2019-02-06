1d0075a38fab494e0cb7672fbca8e84ecfae1dbf
Rafael Olaechea <rolaechea@gsd.uwaterloo.ca>
2019-02-06 11:33:29 -0500

    script for rq2 control

---
+++ execution_scripts/analyzeRQ2FseX264-container.sh
@@ -0,0 +1,10 @@
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 container/train_conf_rep_1.pkl container/test_conf_rep_1.pkl container/traceExecutionTimesForAll.pkl > container/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 container/train_conf_rep_2.pkl container/test_conf_rep_2.pkl container/traceExecutionTimesForAll.pkl | tail -n1 >> container/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 container/train_conf_rep_3.pkl container/test_conf_rep_3.pkl container/traceExecutionTimesForAll.pkl | tail -n1  >> container/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 container/train_conf_rep_4.pkl container/test_conf_rep_4.pkl container/traceExecutionTimesForAll.pkl | tail -n1 >> container/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 container/train_conf_rep_5.pkl container/test_conf_rep_5.pkl container/traceExecutionTimesForAll.pkl | tail -n1  >> container/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 container/train_conf_rep_6.pkl container/test_conf_rep_6.pkl container/traceExecutionTimesForAll.pkl | tail -n1  >> container/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 container/train_conf_rep_7.pkl container/test_conf_rep_7.pkl container/traceExecutionTimesForAll.pkl | tail -n1  >> container/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 container/train_conf_rep_8.pkl container/test_conf_rep_8.pkl container/traceExecutionTimesForAll.pkl | tail -n1  >> container/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 container/train_conf_rep_9.pkl container/test_conf_rep_9.pkl container/traceExecutionTimesForAll.pkl | tail -n1  >> container/results/rq2_fse_control.csv
+python -m FSELearning.AnalyzeRQ2WithFSE  x264 container/train_conf_rep_10.pkl container/test_conf_rep_10.pkl container/traceExecutionTimesForAll.pkl | tail -n1  >> container/results/rq2_fse_control.csv
