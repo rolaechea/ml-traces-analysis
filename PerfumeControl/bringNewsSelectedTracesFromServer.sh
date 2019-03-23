9d0231253ec589f4515d306ac445da5f30812d60
Rafael Olaechea <rolaechea@gsd.uwaterloo.ca>
2019-03-23 15:32:14 -0400

    scripting perfume traces

---
+++ PerfumeControl/bringNewsSelectedTracesFromServer.sh
@@ -0,0 +1,10 @@
+scp rolaechea@ntimber.cs.uwaterloo.ca:~/FaseExperiments/FullTraces/news/conf-327-rep-1-timing.txt ../FullTraces/news/
+scp rolaechea@ntimber.cs.uwaterloo.ca:~/FaseExperiments/FullTraces/news/conf-71-rep-1-timing.txt ../FullTraces/news/
+scp rolaechea@ntimber.cs.uwaterloo.ca:~/FaseExperiments/FullTraces/news/conf-263-rep-1-timing.txt ../FullTraces/news/
+scp rolaechea@ntimber.cs.uwaterloo.ca:~/FaseExperiments/FullTraces/news/conf-1997-rep-1-timing.txt ../FullTraces/news/
+scp rolaechea@ntimber.cs.uwaterloo.ca:~/FaseExperiments/FullTraces/news/conf-1137-rep-1-timing.txt ../FullTraces/news/
+scp rolaechea@ntimber.cs.uwaterloo.ca:~/FaseExperiments/FullTraces/news/conf-401-rep-1-timing.txt ../FullTraces/news/
+scp rolaechea@ntimber.cs.uwaterloo.ca:~/FaseExperiments/FullTraces/news/conf-1267-rep-1-timing.txt ../FullTraces/news/
+scp rolaechea@ntimber.cs.uwaterloo.ca:~/FaseExperiments/FullTraces/news/conf-635-rep-1-timing.txt ../FullTraces/news/
+scp rolaechea@ntimber.cs.uwaterloo.ca:~/FaseExperiments/FullTraces/news/conf-1212-rep-1-timing.txt ../FullTraces/news/
+scp rolaechea@ntimber.cs.uwaterloo.ca:~/FaseExperiments/FullTraces/news/conf-1054-rep-1-timing.txt ../FullTraces/news/
